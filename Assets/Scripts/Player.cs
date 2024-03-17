425e55607a35bdf3b4b6c87a06320028b05dd574
NivorBUD <via4eslav.dubrovin@yandex.ru>
2024-03-17 17:52:29 +0500

    Update move system and add shoot system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
@@ -8,51 +9,53 @@ public class Player : MonoBehaviour
     public static Player Instance { get; private set; }
 
     private Rigidbody2D rb;
-    private float moveSpeed = 10;
-    private float jumpSpeed = 5;
+    private float maxMoveSpeed = 4;
+    private float moveAcceleration = 30;
+    private float jumpForce = 5;
+    private float reboundForce = 6.5f;
     private float moveDirection;
+    private Renderer sprite;
 
     [Header("Grounded")]
-    [SerializeField]private Transform groundCheckPoint;
+    [SerializeField]private Transform groundCheckPoint, centerCheckPoint;
     [SerializeField] private Vector2 checkRadius;
     [SerializeField] private LayerMask whatIsGround;
     private bool isGrounded;
 
     [Header("Jump")]
-    [SerializeField] private float wallJumpTime = 0.2f;
-    [SerializeField] private float wallSlideSpeed = 0.3f;
     [SerializeField] private float wallDistance = 0.52f;
-    private float jumpTime;
     public bool isWallSliding;
     private RaycastHit2D wallCheckHitRight, wallCheckHitLeft;
-    private int extraJumps;
-    [SerializeField] private int extraJumpsValue;
-    
+
+    [Header("Shoot")]
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform shootPlace;
+    [SerializeField] private float recoilForce;
+    [SerializeField] private float rechargeTime;
+    private float timeAfterLastShoot;
+
 
     private void Awake()
     {
         Instance = this;
         rb = GetComponent<Rigidbody2D>();
-        extraJumps = extraJumpsValue;
+        sprite = transform.GetChild(0).GetComponent<Renderer>();
     }
 
     private void Update()
     {
-        if (isGrounded)
-            extraJumps = extraJumpsValue;
+        wallCheckHitRight = Physics2D.Raycast(transform.position, new Vector2(wallDistance, 0), wallDistance, whatIsGround);
+        wallCheckHitLeft = Physics2D.Raycast(transform.position, new Vector2(-wallDistance, 0), wallDistance, whatIsGround);
 
-        if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = Vector2.up * jumpSpeed;
-            extraJumps--;
-        }
-        else if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || isWallSliding) && extraJumps == 0)
-        {
-            rb.velocity = Vector2.up * jumpSpeed;
+            TryToJump();
         }
 
-        wallCheckHitRight = Physics2D.Raycast(groundCheckPoint.position, new Vector2(wallDistance, 0), wallDistance, whatIsGround);
-        wallCheckHitLeft = Physics2D.Raycast(groundCheckPoint.position, new Vector2(-wallDistance, 0), wallDistance, whatIsGround);
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            TryToShoot();
+        }
     }
 
     private void FixedUpdate()
@@ -60,35 +63,45 @@ public class Player : MonoBehaviour
         isGrounded = Physics2D.OverlapBox(groundCheckPoint.position, checkRadius, 0f, whatIsGround);
 
         moveDirection = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
-
-        if ((wallCheckHitRight || wallCheckHitLeft) && !isGrounded && moveDirection != 0)
+        if (isGrounded && moveDirection != 0)
         {
-            isWallSliding = true;
-            jumpTime = Time.time + wallJumpTime;
-        }
-        else if (jumpTime < Time.time)
-        {
-            isWallSliding = false;
-        }
-
-        //первый вариант отскоков от стен
-        if (isWallSliding)
-        {
-            if (wallCheckHitRight && Input.GetKey(KeyCode.D))
+            if ((moveDirection > 0 && rb.velocity.x < maxMoveSpeed) || 
+                (moveDirection < 0 && rb.velocity.x > -maxMoveSpeed))
             {
-                rb.velocity = new Vector2(0, Mathf.Clamp(rb.velocity.y, -wallSlideSpeed, float.MaxValue));
-            }
-            else if (wallCheckHitLeft && Input.GetKey(KeyCode.A))
-            {
-                rb.velocity = new Vector2(0, Mathf.Clamp(rb.velocity.y, -wallSlideSpeed, float.MaxValue));
+                rb.AddForce(Vector2.right * (moveDirection * moveAcceleration), ForceMode2D.Force);
             }
         }
 
-        //немного другой вариант, надо узнать у всех, какой лучше
-        //if (isWallSliding)
-        //{
-        //    rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlideSpeed, float.MaxValue));
-        //}
+        isWallSliding = (wallCheckHitRight || wallCheckHitLeft) && !isGrounded;
+        timeAfterLastShoot += Time.fixedDeltaTime;
+    }
+
+    private void TryToJump()
+    {
+        if (isGrounded)
+        {
+            rb.velocity += Vector2.up * jumpForce;
+        }
+        else if (isWallSliding)
+        {
+            if (wallCheckHitRight)
+            {
+                rb.velocity += new Vector2(-.75f, 1) * reboundForce;
+            }
+            else if (wallCheckHitLeft)
+            {
+                rb.velocity += new Vector2(.75f, 1) * reboundForce;
+            }
+        }
+    }
+
+    private void TryToShoot()
+    {
+        if (timeAfterLastShoot >= rechargeTime)
+        {
+            Instantiate(bulletPrefab, shootPlace.position, shootPlace.rotation);
+            rb.AddForce(Vector2.left * recoilForce, ForceMode2D.Impulse);
+            timeAfterLastShoot = 0;
+        }
     }
 }
