eb1f5202a26911fd77c6f5fc9934f5a2bf05f809
NivorBUD <via4eslav.dubrovin@yandex.ru>
2024-03-17 21:08:26 +0500

    Update movement, rotations and shoots

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,12 +9,13 @@ public class Player : MonoBehaviour
     public static Player Instance { get; private set; }
 
     private Rigidbody2D rb;
-    private float maxMoveSpeed = 4;
-    private float moveAcceleration = 30;
+    private float maxMoveSpeed = 7.5f;
     private float jumpForce = 5;
     private float reboundForce = 6.5f;
     private float moveDirection;
-    private Renderer sprite;
+    private float rotateDirection;
+    private float rotateSpeed = 180;
+    private bool canRotation;
 
     [Header("Grounded")]
     [SerializeField]private Transform groundCheckPoint, centerCheckPoint;
@@ -39,14 +40,21 @@ public class Player : MonoBehaviour
     {
         Instance = this;
         rb = GetComponent<Rigidbody2D>();
-        sprite = transform.GetChild(0).GetComponent<Renderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapBox(groundCheckPoint.position, checkRadius, 0f, whatIsGround);
+
+        isWallSliding = (wallCheckHitRight || wallCheckHitLeft) && !isGrounded;
+        timeAfterLastShoot += Time.fixedDeltaTime;
     }
 
     private void Update()
     {
         wallCheckHitRight = Physics2D.Raycast(transform.position, new Vector2(wallDistance, 0), wallDistance, whatIsGround);
         wallCheckHitLeft = Physics2D.Raycast(transform.position, new Vector2(-wallDistance, 0), wallDistance, whatIsGround);
-
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
             TryToJump();
@@ -56,28 +64,59 @@ public class Player : MonoBehaviour
         {
             TryToShoot();
         }
-    }
-
-    private void FixedUpdate()
-    {
-        isGrounded = Physics2D.OverlapBox(groundCheckPoint.position, checkRadius, 0f, whatIsGround);
 
         moveDirection = Input.GetAxis("Horizontal");
-        if (isGrounded && moveDirection != 0)
+        rotateDirection = Input.GetAxis("Vertical");
+        if (moveDirection != 0 || rotateDirection != 0)
         {
-            if ((moveDirection > 0 && rb.velocity.x < maxMoveSpeed) || 
-                (moveDirection < 0 && rb.velocity.x > -maxMoveSpeed))
+            Move();
+        }
+    }
+
+    private void Move()
+    {
+        if (isGrounded)
+        {
+            if (Math.Abs(rb.velocity.x) < maxMoveSpeed)
             {
-                rb.AddForce(Vector2.right * (moveDirection * moveAcceleration), ForceMode2D.Force);
+                rb.velocity += Vector2.right * moveDirection / 5.0f;
+            }
+
+            if (moveDirection < 0 && transform.rotation.eulerAngles.y == 0)
+            {
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 180, transform.rotation.eulerAngles.z);
+            }
+            else if (moveDirection >= 0 && transform.rotation.eulerAngles.y == 180)
+            {
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0, transform.rotation.eulerAngles.z);
             }
         }
+        //else if(canRotation)
+        //{
+        //    if (rotateDirection > 0)
+        //    {
+        //        transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+        //    }
+        //    else if (rotateDirection < 0)
+        //    {
+        //        transform.Rotate(-Vector3.forward * rotateSpeed * Time.deltaTime);
+        //    }
 
-        isWallSliding = (wallCheckHitRight || wallCheckHitLeft) && !isGrounded;
-        timeAfterLastShoot += Time.fixedDeltaTime;
+        //    if ((transform.rotation.eulerAngles.z % 360 > 0 && transform.rotation.eulerAngles.z % 360 < 90) || 
+        //        (transform.rotation.eulerAngles.z % 360 > 270))
+        //    {
+        //        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 180, transform.rotation.eulerAngles.z);
+        //    }
+        //    else
+        //    {
+        //        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0, transform.rotation.eulerAngles.z);
+        //    }
+        //}
     }
 
     private void TryToJump()
     {
+        canRotation = true;
         if (isGrounded)
         {
             rb.velocity += Vector2.up * jumpForce;
@@ -87,10 +126,12 @@ public class Player : MonoBehaviour
             if (wallCheckHitRight)
             {
                 rb.velocity += new Vector2(-.75f, 1) * reboundForce;
+                transform.rotation = Quaternion.Euler(0, 180, 0);
             }
             else if (wallCheckHitLeft)
             {
                 rb.velocity += new Vector2(.75f, 1) * reboundForce;
+                transform.rotation = Quaternion.Euler(0, 0, 0);
             }
         }
     }
@@ -100,8 +141,17 @@ public class Player : MonoBehaviour
         if (timeAfterLastShoot >= rechargeTime)
         {
             Instantiate(bulletPrefab, shootPlace.position, shootPlace.rotation);
-            rb.AddForce(Vector2.left * recoilForce, ForceMode2D.Impulse);
+            rb.AddRelativeForce(- new Vector3(transform.rotation.x, transform.rotation.y).normalized * recoilForce, ForceMode2D.Impulse);
             timeAfterLastShoot = 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Floor"))
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, 0);
+            canRotation = false;
         }
     }
 }
