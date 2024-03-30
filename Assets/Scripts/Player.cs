380bc37a0ccb4dff7357d7af24c9e002dc3a7d6f
NivorBUD <via4eslav.dubrovin@yandex.ru>
2024-03-30 19:33:41 +0500

    Add slide

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,26 +8,30 @@ public class Player : MonoBehaviour
 {
     public static Player Instance { get; private set; } // singleton
 
-    public float cos;
-    public float sin;
-
     private Rigidbody2D rb;
     private float maxMoveSpeed = 7.5f; // максимальная скорость движения по горизонтали
-    private float jumpForce = 5f; // сила прыжка 
+    private float jumpForce = 8f; // сила прыжка 
     private float reboundForce = 7.5f; //сила прыжка от стены
     public float moveDirection; // направление движения (W/D и стрелки)
-    private float rotateSpeed = 180; // скорость вращения 
+    private float rotateSpeed = 240; // скорость вращения 
+    private BoxCollider2D col;
+    private Vector2 squatColliderSize = new(1.8f, 0.75f);
+    private Vector2 normalColliderSize = new(1.8f, 1.22f);
 
     [Header("Grounded")]
     [SerializeField] private Transform groundCheckPoint;
     [SerializeField] private Vector2 checkRadius;
     [SerializeField] private LayerMask whatIsGround;
     [SerializeField] private LayerMask whatIsWall;
-    public bool isGrounded;
+    [SerializeField] private PhysicsMaterial2D normalMaterial;
+    [SerializeField] private PhysicsMaterial2D squatMaterial;
+    private bool isGrounded;
+    private bool isSquat;
 
     [Header("Jump")]
     [SerializeField] private float wallDistance = 0.52f;
-    public bool isWallSliding;
+    [SerializeField] private float superJumpCoefficient = 1.5f;
+    private bool isWallSliding;
     private RaycastHit2D wallCheckHitRight, wallCheckHitLeft;
 
     [Header("Shoot")]
@@ -37,11 +41,17 @@ public class Player : MonoBehaviour
     [SerializeField] private float rechargeTime;
     private float timeAfterLastShoot;
 
+    [Header("Sprites")]
+    [SerializeField] private Sprite roboSprite;
+    [SerializeField] private Sprite roboSpriteSit;
+    [SerializeField] private SpriteRenderer render;
+
 
     private void Awake()
     {
         Instance = this;
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
     }
 
     private void FixedUpdate()
@@ -56,8 +66,6 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        cos = (float)Math.Cos(transform.rotation.eulerAngles.z * Math.PI / 180.0);
-        sin = (float)Math.Sin(transform.rotation.eulerAngles.z * Math.PI / 180.0);
         wallCheckHitRight = Physics2D.Raycast(transform.position, new Vector2(wallDistance, 0), wallDistance, whatIsWall);
         wallCheckHitLeft = Physics2D.Raycast(transform.position, new Vector2(-wallDistance, 0), wallDistance, whatIsWall);
         
@@ -80,6 +88,8 @@ public class Player : MonoBehaviour
         {
             rb.angularVelocity = 0;
         }
+
+        Squat();
     }
 
     private void Move()
@@ -87,36 +97,24 @@ public class Player : MonoBehaviour
         if (isGrounded)
         {
             rb.angularVelocity = 0;
+
             if (transform.rotation.eulerAngles.z != 0)
             {
                 transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
             }
-            
-            if (Math.Abs(rb.velocity.x) < maxMoveSpeed)
+
+            if (!isSquat && Math.Abs(rb.velocity.x) < maxMoveSpeed)
             {
                 rb.velocity += Vector2.right * moveDirection / 5.0f;
             }
 
-            if (moveDirection <= 0 && transform.rotation.eulerAngles.y != 180)
-            {
-                transform.rotation = Quaternion.Euler(0, 180, 0);
-            }
-            else if (moveDirection > 0 && transform.rotation.eulerAngles.y != 0)
-            {
-                transform.rotation = Quaternion.Euler(0, 0, 0);
-            }
+            Turn();
         }
         else
         {
-            if (moveDirection > 0)
+            if (moveDirection != 0)
             {
-                rb.angularVelocity = rotateSpeed;
-                //transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
-            }
-            else if (moveDirection < 0)
-            {
-                rb.angularVelocity = -rotateSpeed;
-                //transform.Rotate(-Vector3.forward * rotateSpeed * Time.deltaTime);
+                rb.angularVelocity = moveDirection > 0 ? rotateSpeed : -rotateSpeed;
             }
             else
             {
@@ -125,14 +123,45 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void Squat()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            isSquat = true;
+            rb.sharedMaterial = squatMaterial;
+            col.size = squatColliderSize;
+            render.sprite = roboSpriteSit;
+        }
+        else if (isSquat) {
+            isSquat = false;
+            rb.sharedMaterial = normalMaterial;
+            col.size = normalColliderSize;
+            render.sprite = roboSprite;
+        }
+    }
+
+    private void Turn()
+    {
+        if (moveDirection <= 0 && transform.rotation.eulerAngles.y != 180)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if (moveDirection > 0 && transform.rotation.eulerAngles.y != 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+
     private void TryToJump()
     {
         if (isGrounded)
         {
-            rb.velocity += Vector2.up * jumpForce;
+            // прыгаем, если в присяде, то прыжок сильнее в superJumpCoefficient раз
+            rb.velocity += (isSquat ? superJumpCoefficient : 1) * jumpForce * Vector2.up; 
         }
         else if (isWallSliding)
         {
+            // если стена справа, то отпрыгиваем влево, если слева, то вправо
             if (wallCheckHitRight)
             {
                 rb.velocity += new Vector2(-.75f, 1) * reboundForce;
@@ -150,11 +179,11 @@ public class Player : MonoBehaviour
     {
         if (timeAfterLastShoot >= rechargeTime)
         {
-            cos = (float)Math.Cos(transform.rotation.eulerAngles.z * Math.PI / 180.0);
-            sin = (float)Math.Sin(transform.rotation.eulerAngles.z * Math.PI / 180.0);
+            // спавним пулю
             Instantiate(bulletPrefab, shootPlace.position, shootPlace.rotation);
-            //rb.AddRelativeForce(Vector3.left * recoilForce, ForceMode2D.Impulse);
-            rb.AddForce(new Vector3((transform.rotation.eulerAngles.y == 0 ? -1 : 1) * cos, - sin, 0) * recoilForce, ForceMode2D.Impulse);
+            // добавляем отдачу с силой recoilForce
+            rb.AddForce(new Vector3((transform.rotation.eulerAngles.y == 0 ? -1 : 1) * (float)Math.Cos(transform.rotation.eulerAngles.z * Math.PI / 180.0),
+                -(float)Math.Sin(transform.rotation.eulerAngles.z * Math.PI / 180.0), 0) * recoilForce * (isGrounded ? 0.5f : 1), ForceMode2D.Impulse);
             timeAfterLastShoot = 0;
         }
     }
