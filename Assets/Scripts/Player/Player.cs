c3537ff9e9c9dcc031c538d9bd75b45614f1ec23
FyGeR2017 <91911137+FyGeR2017@users.noreply.github.com>
2024-04-26 00:24:35 +0500

    SkyShader, Scene2 and fixes
    Сделай шейдер для градиент неба
    Добавлена Сцена2, реализована основная часть локации
    Фикс для большинства квадратных объектов теперь используется более легкая текстура 256==>16 пискелей.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -238,11 +238,11 @@ public class Player : MonoBehaviour
         if (timeAfterLastShoot >= rechargeTime && !isSquat)
         {
             bool needBost = transform.rotation.eulerAngles.z > 240 && transform.rotation.eulerAngles.z < 300;
-            CamController.cameraShake?.Invoke(10f, 0.1f, 0.1f);
             sounds.PlaySound(3);
             Vector3 recoilVector = new Vector3((transform.rotation.eulerAngles.y == 0 ? -1 : 1) * (float)Math.Cos(transform.rotation.eulerAngles.z * Math.PI / 180.0),
                 -(float)Math.Sin(transform.rotation.eulerAngles.z * Math.PI / 180.0), 0);
-            
+
+            CamController.cameraShake?.Invoke(10f, 0.1f, 0.1f);
             // спавним пулю, надо пофиксить, смотрим влево - летит не туда 
             Instantiate(transform.rotation.y == 0 ? bulletPrefab : bulletReversePrefab, 
                 shootPlace.position, transform.rotation);
