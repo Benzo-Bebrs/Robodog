6b4e7ab598a5ce83f77711742c77740a63453766
FyGeR2017 <91911137+FyGeR2017@users.noreply.github.com>
2024-05-04 11:40:56 +0500

    add transitions between scenes

---
+++ Assets/Scripts/GameScript/EndScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScene : MonoBehaviour
+{
+    public GameObject Fade;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(LoadNextScene());
+        }
+    }
+
+    private IEnumerator LoadNextScene()
+    {
+        Fade.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        var index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(index + 1);
+    }
+}
