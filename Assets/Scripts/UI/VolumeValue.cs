a269baeb325f3cddbe3a0689493f5587d59536ff
AlexLe024 <a.gorlyshkov@gmail.com>
2024-06-01 04:13:11 +0500

    volume settings

--- Assets/Scripts/UI/VolumeValue.cs
+++ Assets/Scripts/UI/VolumeValue.cs
@@ -1,32 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI; // Добавьте это пространство имен, чтобы работать с UI
 
 public class VolumeValue : MonoBehaviour
 {
+    public Slider volumeSlider; // Переменная для ползунка громкости
     public bool isOn;
     private AudioSource audioSrc;
     private float musicVolume = 1f;
 
-    private void VolumeOff()
-    {
-        isOn = true;
-    }
-
     void Start()
     {
         audioSrc = GetComponent<AudioSource>();
+        LoadVolumeSettings();
     }
 
     void Update()
     {
-        audioSrc.volume = musicVolume;   
+        audioSrc.volume = musicVolume;
+        AudioListener.volume = musicVolume;
     }
 
     public void SetVolume(float vol)
     {
         musicVolume = vol;
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+        PlayerPrefs.Save();
+        UpdateVolumeSlider();
     }
 
     public void OnOffSound()
@@ -34,12 +33,38 @@ public class VolumeValue : MonoBehaviour
         if (!isOn)
         {
             AudioListener.volume = 1f;
+            musicVolume = 1f;
             isOn = true;
         }
-        else if (isOn)
+        else
         {
             AudioListener.volume = 0f;
+            musicVolume = 0f;
             isOn = false;
+        }
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+        PlayerPrefs.Save();
+        UpdateVolumeSlider();
+    }
+
+    private void LoadVolumeSettings()
+    {
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            musicVolume = PlayerPrefs.GetFloat("MusicVolume");
+        }
+        else
+        {
+            musicVolume = 1f; // Уровень громкости по умолчанию
+        }
+        UpdateVolumeSlider();
+    }
+
+    private void UpdateVolumeSlider()
+    {
+        if (volumeSlider != null)
+        {
+            volumeSlider.value = musicVolume; // Устанавливаем ползунок в сохраненное положение
         }
     }
 }
